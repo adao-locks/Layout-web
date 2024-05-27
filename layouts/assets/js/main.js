1f7adb9dad52c21fd36fbe95181ce96786576da9
Eduardo Adão Locks <122515706+adao-locks@users.noreply.github.com>
2024-05-27 20:13:51 -0300

    Delete layouts directory

--- layouts/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-function changeIMG() {
-    var imagemProduto = document.querySelector("#imagePrin");
-    if (document.querySelector("#img001")) {
-        imagemProduto.src = "assets/img/Itapema/image2.png";
-    } else if (document.querySelector("#img002")) {
-        imagemProduto.src = "assets/img/Itapema/image.png";
-    }
-}
